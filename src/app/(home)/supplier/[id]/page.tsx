7ca62f06bf51bc58781bcd661ec04454c819cfa1
Tuyt Mai <tranthituyetmai7430@gmail.com>
2024-05-27 02:04:54 +0700

    feat: update supplier detail page

--- src/app/(home)/supplier/[id]/page.tsx
+++ src/app/(home)/supplier/[id]/page.tsx
@@ -5,6 +5,10 @@ import fetchWithToken from "@/utils/fetchWithToken";
 import SupplierFunction from "./supplierFunction";
 import useScreen from "@/hooks/useScreen";
 import TimeLineList from "./TimeLineList";
+import Button from "@/components/Button/Button";
+import { RiPencilLine } from "react-icons/ri";
+import { BsTelephoneFill } from "react-icons/bs";
+import { MdOutlineMail } from "react-icons/md";
 
 export default async function Page({ params: { id } }: PropTypes) {
     const supplierResponse = await fetchWithToken(API.supplier.getDetail(id));
@@ -16,12 +20,43 @@ export default async function Page({ params: { id } }: PropTypes) {
                 <TimeLineList supplierId={id} />
             </div>
             <div className=" py-5 pl-5 relative w-full h-fit flex flex-col gap-5 rounded-lg border-[1px] border-secondary-300">
-                <LabeledText title="Name" value={supplier.name} />
-                <LabeledText title="Email" value={supplier.email} />
-                <LabeledText title="Phone" value={supplier.phone} />
-                <LabeledText title="Address" value={supplier.address} />
-                <LabeledText title="Note" value={supplier.note} />
-                <SupplierFunction id={id} />
+                <div className=" flex flex-row justify-between">
+                    <div>
+                        <div className="flex flex-row gap-4 items-center">
+                            <p className=" text-secondary-600 text-sm">
+                                Supplier name
+                            </p>
+                            <Button
+                                btnType={"secondary"}
+                                className=""
+                                hiddenTitle="Edit"
+                            >
+                                <RiPencilLine size={20} />
+                            </Button>
+                        </div>
+                        <p className=" mt-0">
+                            <span className=" text-2xl font-bold">
+                                {supplier.name}
+                            </span>
+                        </p>
+                    </div>
+                    <div className=" pr-16 mt-4 flex flex-col gap-2">
+                        <div className=" flex flex-row gap-2 items-center">
+                            <BsTelephoneFill size={16} />
+                            <p className=" font-semibold">{supplier.phone}</p>
+                        </div>
+                        <div className=" flex flex-row gap-2 items-center">
+                            <MdOutlineMail size={20} />
+                            <p className=" font-semibold">{supplier.email}</p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <p className=" text-secondary-600 text-sm">Note</p>
+                    <p className=" mt-1">
+                        <span className=" text-base">{supplier.note}</span>
+                    </p>
+                </div>
             </div>
         </>
     );
